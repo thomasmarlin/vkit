f6261e7dc218df98adc5292069dc38a09b89f573
Tom Marlin <Tom@Toms-MacBook-Pro.local>
2015-10-26 22:46:15 -0500

    Removed angular to avoid issues on the PC website

--- js/VkitMain.js
+++ js/VkitMain.js
@@ -1,263 +1,296 @@
 
-var myApp = angular.module('vkit-app', []);
+var MARGIN_LEFT = 0.35;
+var MARGIN_TOP = 0.35;
 
-console.log("AfterLoad!");
+var MAX_PAGE_BOTTOM = 11.0 - MARGIN_LEFT;
+var MAX_PAGE_RIGHT = 8.5 - MARGIN_TOP;
 
-angular.module('vkit-app').controller('VkitMain', ['$scope',  function($scope) {
+var CARD_WIDTH = 2.5;
+
+
+var matchingCards = [];
+var cardsForPdf = [];
+var printedCards = [];
+
+
+
+function expandCollapseInstructions() {
+    var displayMode = jQuery('#instructionsObj').css('display');
+    if (displayMode == "none") {
+        jQuery('#instructionsObj').css('display', 'block');
+    } else {
+        jQuery('#instructionsObj').css('display', 'none');
+    }
+}
+
+function getFilterText() {
+  var textObj = jQuery('#filterText');
+  return textObj.val();
+}
+
+function updateMatchingCards() {
+
+    var filterText = getFilterText();
+    console.log("Filter Change!: " + filterText);
+
+
+    matchingCards.length = 0;
+    for (var i = 0; i < allCardNames.length; i++){
+        var matches = false;
+
+        var lowercaseFilterText = filterText.toLowerCase();
+
+        if ("" == lowercaseFilterText) {
+            matches = true;
+        } else if (-1 != allCardNames[i].toLowerCase().indexOf(lowercaseFilterText)) {
+            matches = true;
+        }
+
+        if (matches) {
+            matchingCards.push(allCardNames[i]);
+        }
+    }
+
+    jQuery('#selectAdds').find('option')
+      .remove();
+
+    for (var i = 0; i < matchingCards.length; i++) {
+        var match = matchingCards[i];
+        console.log("Add card: " + match);
+        jQuery('#selectAdds').append('<option value="' + match + '">' + match + '</option>');
+    }
+
+}
+
+function queueFilterChange() {
+    setTimeout(filterChanged, 250);
+}
+
+function filterChanged() {
+    updateMatchingCards();
+}
+
+function addSelectedCards(isWhiteBorder) {
+
+  // Try to add the cards next to it's duplicates (if exist)
+  $("#selectAdds").find(":selected").each(function() {
+      var cardToAdd = jQuery(this).val()
+
+      if (isWhiteBorder) {
+          cardToAdd += " (WB)";
+      }
+
+      var added = false;
+      for (var j = 0; j < cardsForPdf.length; j++) {
+          if (cardsForPdf[j] == cardToAdd) {
+              cardsForPdf.splice(j, 0, cardToAdd);
+              added = true;
+              break;
+          }
+      }
+
+      if (!added) {
+          cardsForPdf.push(cardToAdd);
+      }
+
+      jQuery('#selectedRemoves').find('option')
+        .remove();
+
+      for (var i = 0; i < cardsForPdf.length; i++) {
+          var match = cardsForPdf[i];
+          console.log("Add card: " + match);
+          jQuery('#selectedRemoves').append('<option value="' + match + '">' + match + '</option>');
+      }
+
+  });
+
+}
+
+function removeSelectedCards() {
+    $("#selectedRemoves").find(":selected").each(function() {
+        var cardToRemove = jQuery(this).val();
+        for (var j = 0; j < cardsForPdf.length; j++) {
+            if (cardsForPdf[j] == cardToRemove) {
+                jQuery(this).remove();
+                cardsForPdf.splice(j, 1);
+                break;
+            }
+        }
+    });
+}
+
+
+function convertImgToBase64(isWhiteBorder, url, callback)
+{
+  var canvas = document.createElement('canvas');
+  var ctx = canvas.getContext('2d');
+  img = document.createElement('img'),
+  img.src = url;
+  img.onload = function() {
+    canvas.height = img.height;
+    canvas.width = img.width;
+    var context = canvas.getContext('2d');
+
+    context.drawImage(img, 0, 0);
+
+    if (isWhiteBorder) {
+        convertCanvasToWhiteBorder2(canvas);
+    }
+
+    var dataURL = canvas.toDataURL('image/jpeg');
+    var aspectRatio = canvas.height / canvas.width;
+    callback(dataURL, aspectRatio);
+    canvas = null;
+  };
+  img.onerror = function() {
+      console.log("Failed ot open image: " + url);
+      callback(null);
+  }
+
+}
+
+function sortCards(originalList, fullTemplates, halfSlips){
+  for (var i = 0; i < originalList.length; i++) {
+      var card = originalList[i];
+      if (card.aspectRatio > 1) {
+          // Card is taller than wide
+          fullTemplates.push(card);
+      } else {
+          halfSlips.push(card);
+      }
+  }
+}
+
+function setPrintPoint(pointObj, top, left, bottom, right) {
+  pointObj.top = top;
+  pointObj.left = left;
+  pointObj.bottom = bottom;
+  pointObj.right = right;
+}
+
+function printCards(doc, cardsToPrint, lastPrintPoint) {
+
+  for (var i = 0; i < cardsToPrint.length; i++) {
+      var card = cardsToPrint[i];
+
+      var calculatedHeight = CARD_WIDTH * card.aspectRatio;
+      //console.log("calculatedHeight: " + calculatedHeight);
+
+      var nextTop = lastPrintPoint.top;
+      var nextLeft = lastPrintPoint.right;
+      var addedPageOrRow = false;
+
+      // If this card exceeds the bottom, add a new page
+      if ((nextTop + calculatedHeight) > MAX_PAGE_BOTTOM) {
+          // Won't fit on page!  Add a new page!
+          //console.log("Next bottom would have been off-page. Figuring out to adapt!");
+          doc.addPage();
+          addedPageOrRow = true;
+          nextTop = MARGIN_TOP;
+          nextLeft = MARGIN_LEFT;
+      }
+
+      // If this card will exceed the width, add a new row OR a new page if needed
+      if ((nextLeft + CARD_WIDTH) > MAX_PAGE_RIGHT) {
+          //console.log("Next right edge would have been off screen. Figuring out how to adapt!");
+          nextTop = lastPrintPoint.bottom;
+
+          // Need to add a new row
+          if ((nextTop + calculatedHeight) < MAX_PAGE_BOTTOM) {
+              // Card will fit in the page in the next rows
+              //console.log("Adding new row!");
+              nextTop = lastPrintPoint.bottom;
+              nextLeft = MARGIN_LEFT;
+              addedPageOrRow = true;
+          } else {
+              // Need a whole new page
+              //console.log("Adding new page!");
+              doc.addPage()
+              nextTop = MARGIN_TOP;
+              nextLeft = MARGIN_LEFT;
+          }
+      } else {
+          // Card will fit in this row on this page!  No adjustments needed.
+      }
+
+      if (card.dataUrl != null){
+          doc.addImage(card.dataUrl, 'jpeg', nextLeft, nextTop, CARD_WIDTH, calculatedHeight);
+      }
+
+      // 'bottom' of last card can't be any higher than the lowest card in the current row
+      var bottomOfLastPrintedCard = nextTop + calculatedHeight;
+      if (!addedPageOrRow) {
+          bottomOfLastPrintedCard = Math.max(bottomOfLastPrintedCard, lastPrintPoint.bottom);
+      }
+      // Adjust the print-point based on the card we just added
+      setPrintPoint(lastPrintPoint, nextTop, nextLeft,
+                                    bottomOfLastPrintedCard, nextLeft + CARD_WIDTH);
+
+  }
+
+}
+
+
+function generatePdf() {
+
+    var doc = new jsPDF('portrait', 'in', 'a4');
+
+    var cardsWithSizes = [];
+
+    function addNextCard(currentCardIndex) {
+        if (currentCardIndex == cardsForPdf.length) {
+
+          var halfSlips = [];
+          var fullTemplates = [];
+          sortCards(cardsWithSizes, fullTemplates, halfSlips);
+
+          var cardsInCurrentRow = 0;
+          var rowsPrinted = 0;
+
+          var lastPrintPoint = {
+              left: MARGIN_LEFT,
+              top: MARGIN_TOP,
+              right: MARGIN_LEFT,
+              bottom: MARGIN_TOP
+          }
+          printCards(doc, fullTemplates, lastPrintPoint);
+          printCards(doc, halfSlips, lastPrintPoint);
+
+          doc.output('save', 'vkitPdf.pdf');
+
+        } else {
+
+          var cardName = cardsForPdf[currentCardIndex];
+          var isWhiteBorder = (-1 != cardName.indexOf(" (WB)"));
+          var cardPath = allCardImages[cardName];
+          console.log("image: " + cardPath );
+
+          var imgData = convertImgToBase64(isWhiteBorder, cardPath, function(dataUrl, aspectRatio){
+
+              cardsWithSizes.push( {
+                cardPath: cardPath,
+                dataUrl: dataUrl,
+                aspectRatio: aspectRatio
+              })
+
+              addNextCard(currentCardIndex+1);
+          });
+        }
+    }
+
+    addNextCard(0);
+}
+
+
+jQuery(document).ready(function(){
+
+    console.log("After Loaded");
 
     console.log(JSON.stringify(allCardNames));
-
-    var MARGIN_LEFT = 0.35;
-    var MARGIN_TOP = 0.35;
-
-    var MAX_PAGE_BOTTOM = 11.0 - MARGIN_LEFT;
-    var MAX_PAGE_RIGHT = 8.5 - MARGIN_TOP;
-
-    var CARD_WIDTH = 2.5;
-
-
-    $scope.vkitModel = {
-      filterText: ""
-    };
-    $scope.matchingCards = [];
-    $scope.cardsForPdf = [];
-    $scope.printedCards = [];
-
-
-    $scope.expandCollapseInstructions = function() {
-        var displayMode = jQuery('#instructionsObj').css('display');
-        if (displayMode == "none") {
-            jQuery('#instructionsObj').css('display', 'block');
-        } else {
-            jQuery('#instructionsObj').css('display', 'none');
-        }
-    }
-
-    function updateMatchingCards() {
-        console.log("Filter Change!: " + $scope.vkitModel.filterText);
-
-        $scope.matchingCards.length = 0;
-        for (var i = 0; i < allCardNames.length; i++){
-            var matches = false;
-
-            var lowercaseFilterText = $scope.vkitModel.filterText.toLowerCase();
-
-            if ("" == lowercaseFilterText) {
-                matches = true;
-            } else if (-1 != allCardNames[i].toLowerCase().indexOf(lowercaseFilterText)) {
-                matches = true;
-            }
-
-            if (matches) {
-                $scope.matchingCards.push(allCardNames[i]);
-            }
-        }
-
-        //console.log("Match size: " + $scope.matchingCards.length)
-    }
-
-    $scope.filterChanged = function() {
-        updateMatchingCards();
-    }
-
-    $scope.addSelectedCards = function(isWhiteBorder) {
-        // Try to add the cards next to it's duplicates (if exist)
-        for (var i = 0; i < $scope.vkitModel.selectedAdds.length; i++) {
-            var cardToAdd = $scope.vkitModel.selectedAdds[i];
-
-            if (isWhiteBorder) {
-                cardToAdd += " (WB)";
-            }
-
-            var added = false;
-            for (var j = 0; j < $scope.cardsForPdf.length; j++) {
-                if ($scope.cardsForPdf[j] == cardToAdd) {
-                    $scope.cardsForPdf.splice(j, 0, cardToAdd);
-                    added = true;
-                    break;
-                }
-            }
-
-            if (!added) {
-              $scope.cardsForPdf.push(cardToAdd);
-            }
-        }
-    }
-
-    $scope.removeSelectedCards = function() {
-        for (var i = 0; i < $scope.vkitModel.selectedRemoves.length; i++) {
-            for (var j = 0; j < $scope.cardsForPdf.length; j++) {
-                if ( $scope.cardsForPdf[j] == $scope.vkitModel.selectedRemoves[i]) {
-                    $scope.cardsForPdf.splice(j, 1);
-                    break;
-                }
-            }
-        }
-    }
-
-    function convertImgToBase64(isWhiteBorder, url, callback)
-    {
-        var canvas = document.createElement('canvas');
-        var ctx = canvas.getContext('2d');
-        img = document.createElement('img'),
-        img.src = url;
-        img.onload = function() {
-          canvas.height = img.height;
-          canvas.width = img.width;
-          var context = canvas.getContext('2d');
-
-          context.drawImage(img, 0, 0);
-
-          if (isWhiteBorder) {
-              convertCanvasToWhiteBorder2(canvas);
-          }
-
-          var dataURL = canvas.toDataURL('image/jpeg');
-          var aspectRatio = canvas.height / canvas.width;
-          callback(dataURL, aspectRatio);
-          canvas = null;
-        };
-        img.onerror = function() {
-            console.log("Failed ot open image: " + url);
-            callback(null);
-        }
-
-    }
-
-    function sortCards(originalList, fullTemplates, halfSlips){
-        for (var i = 0; i < originalList.length; i++) {
-            var card = originalList[i];
-            if (card.aspectRatio > 1) {
-                // Card is taller than wide
-                fullTemplates.push(card);
-            } else {
-                halfSlips.push(card);
-            }
-        }
-    }
-
-    function setPrintPoint(pointObj, top, left, bottom, right) {
-        pointObj.top = top;
-        pointObj.left = left;
-        pointObj.bottom = bottom;
-        pointObj.right = right;
-    }
-
-    function printCards(doc, cardsToPrint, lastPrintPoint) {
-
-        for (var i = 0; i < cardsToPrint.length; i++) {
-            var card = cardsToPrint[i];
-
-            var calculatedHeight = CARD_WIDTH * card.aspectRatio;
-            //console.log("calculatedHeight: " + calculatedHeight);
-
-            var nextTop = lastPrintPoint.top;
-            var nextLeft = lastPrintPoint.right;
-            var addedPageOrRow = false;
-
-            // If this card exceeds the bottom, add a new page
-            if ((nextTop + calculatedHeight) > MAX_PAGE_BOTTOM) {
-                // Won't fit on page!  Add a new page!
-                //console.log("Next bottom would have been off-page. Figuring out to adapt!");
-                doc.addPage();
-                addedPageOrRow = true;
-                nextTop = MARGIN_TOP;
-                nextLeft = MARGIN_LEFT;
-            }
-
-            // If this card will exceed the width, add a new row OR a new page if needed
-            if ((nextLeft + CARD_WIDTH) > MAX_PAGE_RIGHT) {
-                //console.log("Next right edge would have been off screen. Figuring out how to adapt!");
-                nextTop = lastPrintPoint.bottom;
-
-                // Need to add a new row
-                if ((nextTop + calculatedHeight) < MAX_PAGE_BOTTOM) {
-                    // Card will fit in the page in the next rows
-                    //console.log("Adding new row!");
-                    nextTop = lastPrintPoint.bottom;
-                    nextLeft = MARGIN_LEFT;
-                    addedPageOrRow = true;
-                } else {
-                    // Need a whole new page
-                    //console.log("Adding new page!");
-                    doc.addPage()
-                    nextTop = MARGIN_TOP;
-                    nextLeft = MARGIN_LEFT;
-                }
-            } else {
-                // Card will fit in this row on this page!  No adjustments needed.
-            }
-
-            if (card.dataUrl != null){
-                doc.addImage(card.dataUrl, 'jpeg', nextLeft, nextTop, CARD_WIDTH, calculatedHeight);
-            }
-
-            // 'bottom' of last card can't be any higher than the lowest card in the current row
-            var bottomOfLastPrintedCard = nextTop + calculatedHeight;
-            if (!addedPageOrRow) {
-                bottomOfLastPrintedCard = Math.max(bottomOfLastPrintedCard, lastPrintPoint.bottom);
-            }
-            // Adjust the print-point based on the card we just added
-            setPrintPoint(lastPrintPoint, nextTop, nextLeft,
-                                          bottomOfLastPrintedCard, nextLeft + CARD_WIDTH);
-
-        }
-
-    }
-
-
-    $scope.generatePdf = function() {
-        var doc = new jsPDF('portrait', 'in', 'a4');
-
-        var cardsWithSizes = [];
-
-        function addNextCard(currentCardIndex) {
-            if (currentCardIndex == $scope.cardsForPdf.length) {
-
-              var halfSlips = [];
-              var fullTemplates = [];
-              sortCards(cardsWithSizes, fullTemplates, halfSlips);
-
-              var cardsInCurrentRow = 0;
-              var rowsPrinted = 0;
-
-              var lastPrintPoint = {
-                  left: MARGIN_LEFT,
-                  top: MARGIN_TOP,
-                  right: MARGIN_LEFT,
-                  bottom: MARGIN_TOP
-              }
-              printCards(doc, fullTemplates, lastPrintPoint);
-              printCards(doc, halfSlips, lastPrintPoint);
-
-              doc.output('save', 'vkitPdf.pdf');
-
-            } else {
-
-              var cardName = $scope.cardsForPdf[currentCardIndex];
-              var isWhiteBorder = (-1 != cardName.indexOf(" (WB)"));
-              var cardPath = allCardImages[cardName];
-              console.log("image: " + cardPath );
-
-              var imgData = convertImgToBase64(isWhiteBorder, cardPath, function(dataUrl, aspectRatio){
-
-                  cardsWithSizes.push( {
-                    cardPath: cardPath,
-                    dataUrl: dataUrl,
-                    aspectRatio: aspectRatio
-                  })
-
-                  addNextCard(currentCardIndex+1);
-              });
-            }
-        }
-
-        addNextCard(0);
-    }
 
     updateMatchingCards();
 
     // Remove the default wordpress entry-header on swccgpc
     jQuery('.entry-header').css('display', 'none');
 
-}]);
+});
--- vkit-app.php
+++ vkit-app.php
@@ -31,8 +31,8 @@ function initVkit(){
   <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">';
   */
 
-  echo '<script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.28/angular.js"></script>
-  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+  //echo '<script src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.28/angular.js"></script>
+  echo '<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/jspdf/1.1.135/jspdf.min.js"></script>
   <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">';
 
@@ -67,8 +67,103 @@ function initVkit(){
 
   echo '</script>';
 
-  echo '<div ng-app="vkit-app" ng-controller="VkitMain" ng-include="\'' . plugins_url('/vkit-app/js/vkit-app.html') . '\'"></div>';
+  echo "<!DOCTYPE html>
+    <meta charset='utf-8'>
 
+    <!-- Prerequisites:
+    <script src='//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js'></script>
+
+    <script src='//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js'></script>
+    <script src='//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.12.0/ui-bootstrap-tpls.js'></script>
+    <script src='//cdnjs.cloudflare.com/ajax/libs/masonry/3.3.1/masonry.pkgd.min.js'></script>
+
+    <link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css'>
+
+    -->
+
+    <div>
+
+        <h2 style='float:left; font-weight:bold; font-size:22px'>Welcome to the new VKit!</h2>
+
+        <!-- Instructions on the right side -->
+        <div class='row' style='width:100%'>
+          <div class='col-md-5' style='float:right'>
+            <h4 style='float:right; text-decoration:underline; font-style:italic' onclick='expandCollapseInstructions()'> + Click For Instructions </h4>
+            <div style='clear:both'></div>
+          </div>
+        </div>
+
+        <div style='clear:both'></div>
+
+        <!-- Instructions (collapsble per above) -->
+        <div id='instructionsObj' style='border:thin solid #DADADA;margin:20px; position:relative; display:none'>
+            <div style='position:absolute; top: 10px; right: 10px;'  onclick='expandCollapseInstructions()'>[Hide]</div>
+            <div style='margin:20px'>
+                <div> - The cards on the left are all of the cards available. </div>
+                <div> - The cards on the right are all of the cards you have selected to print. </div>
+                <div> - Type part of a card name into the 'filter' to limit the cards you see on the left.</div>
+                <div> - Select a card on the left and press 'Add >>' to add it to the list on the right. Likewise to remove a card </div>
+                <div> - Click 'Generate PDF' when you are ready. </div>
+                <div style='text-decoration:underline; font-weight:bold'>The PDF may take a little while to generate. When it is ready, Your download should start automatically</div>
+            </div>
+        </div>
+
+
+        <!-- Row: Labels -->
+        <div class='row' style='width:100%'>
+          <div class='col-md-5'>
+              <label>Filter Cards:</label>
+              <div style='clear:both'></div>
+              <input type='text' id='filterText' onkeypress='queueFilterChange()' onchange='filterChanged()' style='width:100%'>
+              <div style='clear:both;height:15px;width:100%'></div>
+          </div>
+        </div>
+
+
+        <!-- Row: Actual controls -->
+        <div class='row' style='height:100%; width:100%'>
+            <div class='col-md-5'>
+                <select multiple id='selectAdds' size=20 style='width:100%;'>
+                  <!--<option ng-repeat='x in matchingCards' value='{{x}}'>{{x}}</option>-->
+                </select>
+            </div>
+
+            <!-- Middle Pane:  Add/Remove buttons -->
+            <div class='col-md-2'>
+                <div>
+                    <button class='btn btn-info btn-block' onclick='addSelectedCards(false)'>Add (BB)>></button>
+                    <div style='height:10px'></div>
+                    <button class='btn btn-info btn-block' onclick='addSelectedCards(true)'>Add (WB)>></button>
+                    <div style='height:10px'></div>
+                    <button class='btn btn-danger btn-block' onclick='removeSelectedCards()'> &lt;&lt; Remove</button>
+                </div>
+            </div>
+
+            <!-- Right Pane:  Added Cards -->
+            <div class='col-md-5'>
+                <select multiple id='selectedRemoves' size=20 style='width:100%;height:100%'>
+                  <!--<option ng-repeat='x in cardsForPdf track by $index' value='{{x}}'>{{x}}</option>-->
+                </select>
+            </div>
+        </div>
+
+
+        <!-- Row: Bottom Buttons -->
+        <div class='row' style='width:100%'>
+          <div class='col-md-5'>
+          </div>
+          <div class='col-md-2'>
+          </div>
+            <div class='col-md-5'>
+                <div style='width:100%;height:10px'></div>
+                <button class='btn btn-success' style='float:right' onclick='generatePdf()'>Generate PDF</button>
+            </div>
+        </div>
+
+
+        <div style='color: red; font-style:italic'>This page is still under construction.  For issues or requests, please post in the forums.</div>
+
+    </div>";
 }
 
 ?>
